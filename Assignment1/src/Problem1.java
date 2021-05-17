ce433d6bab6fa13a6c6706ad49b99746b590de45
KidusMT <kidusmamuye@gmail.com>
2021-05-17 17:41:42 -0500

    update on problem1 solution

--- Assignment1/src/Problem1.java
+++ Assignment1/src/Problem1.java
@@ -5,20 +5,22 @@
 public class Problem1 {
 
     public static void main(String[] args) {
-        System.out.println(sumFound(new int[]{1,2,3,4,5,6}, 10));
+        System.out.println(sumFound(new int[]{1, 4, 2, 3}, 5));  // case 1
+        System.out.println(sumFound(new int[]{3, 3, 4, 7}, 6));  // case 2
+        System.out.println(sumFound(new int[]{1}, 2));           // case 3
     }
 
     public static boolean sumFound(int[] arr, int z) {
-        if(arr==null || arr.length < 2) {
+        if (arr == null || arr.length < 2) {
             return false;
         }
         //implement
-        for (int i = 0; i <arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-               if(arr[i]+arr[j] == z){
-//                   System.out.format("arr[i]: %d arr[j]: %d", arr[i], arr[j]);
-                   return true;
-               }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] != arr[j] && arr[i] + arr[j] == z) {
+//                    System.out.format("arr[i]: %d arr[j]: %d", arr[i], arr[j]);
+                    return true;
+                }
             }
         }
 
