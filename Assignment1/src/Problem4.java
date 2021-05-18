d25347c86735215222aee348a488823a0684fad4
KidusMT <kidusmamuye@gmail.com>
2021-05-17 22:38:45 -0500

    updated solution 4

--- Assignment1/src/Problem4.java
+++ Assignment1/src/Problem4.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * @author KidusMT
@@ -7,10 +8,27 @@ import java.util.Arrays;
  */
 public class Problem4 {
     public static void main(String[] args) {
-        sort(new ArrayList<>(Arrays.asList(1, 3, 9, 4, 8, 5)));
+        sort1(new ArrayList<>(Arrays.asList(1, 3, 9, 4, 8, 5)));
+        sort2(new LinkedList<>(Arrays.asList(1, 3, 9, 4, 8, 5)));
     }
 
-    public static void sort(ArrayList<Integer> list) {
+    // for ArrayList
+    public static void sort1(ArrayList<Integer> list) {
+        //implement
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(i)) {
+                    int temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        System.out.println(list);
+    }
+
+    // for LinkedList
+    public static void sort2(LinkedList<Integer> list){
         //implement
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
