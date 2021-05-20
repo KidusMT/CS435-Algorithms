a9b8f559531eeba0cbe0d461657f5ac935e7d790
KidusMT <kidusmamuye@gmail.com>
2021-05-19 23:02:17 -0500

    solution to problem6

--- Assignment1B/src/Problem6.java
+++ Assignment1B/src/Problem6.java
@@ -7,30 +7,25 @@ import java.util.*;
 public class Problem6 {
 
     public static void main(String[] args) {
-//        System.out.println(powerSet(Arrays.asList(1, 2)));
+        System.out.println(powerSet(Arrays.asList(1, 2)));
     }
 
-//    public static List<Set> powerSet(List<Integer> list) {
-//        //implement
-//
-//    }
+    public static List<Set<Integer>> powerSet(List<Integer> list) {
+        List<Set<Integer>> wholeSet= new ArrayList<>() ;
+        Set<Integer> subsetCreated = new HashSet<Integer>();
+        subsetCreator(wholeSet,subsetCreated, list,0);
+        return wholeSet;
+    }
 
-//    public static List<Set<Integer>> powerSet(List<Integer> list) {
-//        List<Set<Integer>> wholeSet= new ArrayList<>() ;
-//        Set<Integer> subsetCreated = new HashSet<Integer>();
-//        subsetCreator(wholeSet,subsetCreated, list,0);
-//        return wholeSet;
-//    }
-//
-//    private static void subsetCreator(List<Set<Integer>> wholeSet, Set<Integer> subsetCreated, List<Integer> list, int start) {
-//        wholeSet.add(new HashSet<>(subsetCreated));
-//        System.out.println("===>wholeSet: "+wholeSet);
-//        for (int i=start;i<list.size();i++){
-//            subsetCreated.add(list.get(i));
-//            System.out.format("addedItem: %s\n",list.get(i));
-//            subsetCreator(wholeSet,subsetCreated,list,i+1);
-//            System.out.format("removedItem: %s\n",list.get(subsetCreated.size()-1));
-//            subsetCreated.remove(subsetCreated.size()-1);
-//        }
-//    }
+    private static void subsetCreator(List<Set<Integer>> wholeSet, Set<Integer> subsetCreated, List<Integer> list, int start) {
+        wholeSet.add(new HashSet<>(subsetCreated));
+        System.out.println("===>wholeSet: "+wholeSet);
+        for (int i=start;i<list.size();i++){
+            subsetCreated.add(list.get(i));
+            System.out.format("addedItem: %s\n",list.get(i));
+            subsetCreator(wholeSet,subsetCreated,list,i+1);
+            System.out.format("removedItem: %s\n",list.get(subsetCreated.size()-1));
+            subsetCreated.remove(subsetCreated.size()-1);
+        }
+    }
 }
